0c43c617d63bcfb865897d47a237d5b82b6d9946
DanieleAntonaci <danieleanto96@gmail.com>
2022-11-19 18:49:05 +0100

    Output num esatti

--- main.js
+++ main.js
@@ -26,27 +26,32 @@ btn.addEventListener('click', function () {
         container.innerHTML = "";
         input.classList.remove('hidden');
         btnResult.classList.remove('hidden');
-
-
-        btnResult.addEventListener('click', function () {
-            console.log(input.value);
-            arrayResult.push(input.value);
-
-            if (5 === arrayResult.length) {
-                win = true
-                for (let i = 0; i < numGame.length; i++) {
-                    if (numGame[i] != arrayResult[i]) {
-                        win = false
-                    }
-                }
-                if (win) {
-                    result.innerHTML = 'Hai vinto!'
-                } else if (!win) {
-                    result.innerHTML = 'Hai perso!';
-                }
-            }
-        })
     }, 3000);
+
+    btnResult.addEventListener('click', function () {
+        console.log(input.value);
+        arrayResult.push(input.value);
+
+        if (5 === arrayResult.length) {
+            input.classList.add('hidden');
+            btnResult.classList.add('hidden');
+            win = true;
+
+            for (let i = 0; i < numGame.length; i++) {
+                if (numGame[i] != arrayResult[i]) {
+                    win = false
+                }
+                else {
+                    container.append(arrayResult[i] + '; ');
+                }
+            };
+            if (win) {
+                result.innerHTML = 'Hai vinto!'
+            } else if (!win) {
+                result.innerHTML = 'Hai perso!';
+            }
+        }
+    })
 });
 
 
