a1634e8e7d44b8732997b163d2355b4889602ead
DanieleAntonaci <danieleanto96@gmail.com>
2022-11-19 18:13:55 +0100

    Risolto bug eaggiunta css

--- main.js
+++ main.js
@@ -16,23 +16,37 @@ btn.addEventListener('click', function () {
     container.append(createDiv);
     console.log(numGame);
     arrayResult = [];
+    result.innerHTML = '';
+    input.classList.add('hidden');
+    btnResult.classList.add('hidden');
+    let win;
 
 
     setTimeout(() => {
         container.innerHTML = "";
+        input.classList.remove('hidden');
+        btnResult.classList.remove('hidden');
+
+
         btnResult.addEventListener('click', function () {
             console.log(input.value);
-            arrayResult.push(input.value)
-            if (numGame.length === arrayResult.length)
+            arrayResult.push(input.value);
+
+            if (5 === arrayResult.length) {
+                win = true
                 for (let i = 0; i < numGame.length; i++) {
-                    if (numGame[i] == arrayResult[i]) {
-                        result.innerHTML = 'Hai vinto!'
-                    } else {
-                        result.innerHTML = 'Hai perso!';
+                    if (numGame[i] != arrayResult[i]) {
+                        win = false
                     }
                 }
+                if (win) {
+                    result.innerHTML = 'Hai vinto!'
+                } else if (!win) {
+                    result.innerHTML = 'Hai perso!';
+                }
+            }
         })
-    }, 30000);
+    }, 3000);
 });
 
 
